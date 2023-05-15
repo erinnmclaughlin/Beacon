3e7e1a3e18e69fc6111423b4f8647e89512cb922
emclaughlin@lighthouseinstruments.com <emclaughlin@lighthouseinstruments.com>
2023-05-15 14:56:17 -0400

    added web app

---
+++ src/UI/BeaconUI.DesktopApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BeaconUI.DesktopApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ src/UI/BeaconUI.DesktopApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BeaconUI.DesktopApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
