95a28e872910a6ca9096dd3d9ef7a2da13400e51
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-05-24 18:40:19 -0400

    start adding register tests

---
+++ tests/Server/Beacon.API.IntegrationTests/Endpoints/Auth/RegisterTests.cs
@@ -0,0 +1,34 @@
+﻿using Beacon.Common.Auth.Register;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Beacon.API.IntegrationTests.Endpoints.Auth;
+
+public class RegisterTests : IClassFixture<BeaconTestApplicationFactory>
+{
+    private readonly BeaconTestApplicationFactory _factory;
+    private readonly HttpClient _httpClient;
+
+    public RegisterTests(BeaconTestApplicationFactory factory)
+    {
+        _factory = factory;
+        _httpClient = _factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Register_ShouldFail_IfEmailIsTaken()
+    {
+        await _factory.SeedDbWithUserData("test@test.com", "test", "pwd12345");
+
+        var response = await _httpClient.PostAsJsonAsync("api/auth/register", new RegisterRequest
+        {
+            EmailAddress = "test@test.com",
+            DisplayName = "someValidName",
+            Password = "someValidPassword"
+        });
+
+        response.IsSuccessStatusCode.Should().BeFalse();
+        response.StatusCode.Should().Be(HttpStatusCode.UnprocessableEntity);
+    }
+}
