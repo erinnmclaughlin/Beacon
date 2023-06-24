091994f8b3f74a08b8bebb3944abb05aa746dd79
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-23 20:21:09 -0400

    cancel/complete project tests

---
+++ tests/Beacon.IntegrationTests/EndpointTests/Projects/CompleteProjectTests.cs
@@ -0,0 +1,36 @@
+﻿using Beacon.App.Entities;
+using Beacon.App.ValueObjects;
+using Beacon.Common.Memberships;
+
+namespace Beacon.IntegrationTests.EndpointTests.Projects;
+
+[Collection("ProjectTests")]
+public class CompleteProjectTests : EndpointTestBase
+{
+    public CompleteProjectTests(BeaconTestApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task CompleteProject_SucceedsWhenRequestIsValid()
+    {
+        AddTestAuthorization(LaboratoryMembershipType.Admin);
+        var projectId = Guid.NewGuid();
+        var client = CreateClient(db =>
+        {
+            db.Projects.Add(new Project
+            {
+                Id = projectId,
+                CustomerName = "Test Customer",
+                ProjectCode = new ProjectCode("TST", 1),
+                CreatedById = TestData.DefaultUser.Id,
+                LaboratoryId = TestData.DefaultLaboratory.Id
+            });
+
+            db.SaveChanges();
+        });
+
+        var response = await client.PostAsync($"api/projects/{projectId}/complete", null);
+        response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+}
