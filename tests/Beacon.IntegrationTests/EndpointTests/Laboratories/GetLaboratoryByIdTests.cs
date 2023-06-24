38959616a1678afe63ae67ee521c42e29a78775d
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-23 20:14:40 -0400

    s'more tests

---
+++ tests/Beacon.IntegrationTests/EndpointTests/Laboratories/GetLaboratoryByIdTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Beacon.IntegrationTests.EndpointTests.Laboratories;
+
+[Collection("LaboratoryTests")]
+public class GetLaboratoryByIdTests : EndpointTestBase
+{
+    public GetLaboratoryByIdTests(BeaconTestApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task GetLaboratoryById_SucceedsWhenUserIsMember()
+    {
+        AddTestAuthorization(Common.Memberships.LaboratoryMembershipType.Member);
+
+        var response = await CreateClient().GetAsync($"api/laboratories/{TestData.DefaultLaboratory.Id}");
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+}
