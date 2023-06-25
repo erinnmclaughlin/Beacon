1eeb211b689463b0c324d98eb1b1138732fd8e1d
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-25 15:05:21 -0400

    test fix

--- tests/Beacon.WebApp.IntegrationTests/BeaconTestContext.cs
+++ tests/Beacon.WebApp.IntegrationTests/BeaconTestContext.cs
@@ -6,7 +6,7 @@ public abstract class BeaconTestContext : TestContext
 {
     protected void SetupCoreServices()
     {
-        Services.AddBeaconUI();
+        Services.AddBeaconUI("localhost");
         this.AddBlazoredLocalStorage();
         JSInterop.SetupModule("./_content/Blazored.Modal/BlazoredModal.razor.js");
         JSInterop.SetupModule("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
