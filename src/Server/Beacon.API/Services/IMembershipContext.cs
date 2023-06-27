3d5fa70aa39ec9966351005e4d8cf0f13dff91b7
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-26 23:34:48 -0400

    remove unused thing

--- src/Server/Beacon.API/Services/IMembershipContext.cs
+++
@@ -1,7 +0,0 @@
-﻿using Beacon.Common.Models;
-
-namespace Beacon.API.Services;
-public interface IMembershipContext
-{
-    Task<LaboratoryMembershipType?> GetMembershipTypeAsync(CancellationToken ct = default);
-}
