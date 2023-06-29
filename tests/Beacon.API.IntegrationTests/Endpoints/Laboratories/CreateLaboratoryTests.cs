228bb4a93918f16ed50ee83a80c0be51bcec6304
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-06-28 22:38:11 -0400

    wip

---
+++ tests/Beacon.API.IntegrationTests/Endpoints/Laboratories/CreateLaboratoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Beacon.Common.Requests.Auth;
+using Beacon.Common.Requests.Laboratories;
+using System.Net;
+
+namespace Beacon.API.IntegrationTests.Endpoints.Laboratories;
+
+public sealed class CreateLaboratoryTests : TestBase
+{
+    public CreateLaboratoryTests(TestFixture fixture) : base(fixture)
+    {
+    }
+
+    [Fact(DisplayName = "Create lab succeeds when request is valid")]
+    public async Task CreateLab_ShouldSucceed_WhenRequestIsValid()
+    {
+        SetCurrentUser(TestData.AdminUser.Id);
+
+        var response = await PostAsync("api/laboratories", new CreateLaboratoryRequest
+        {
+            LaboratoryName = "Test Lab"
+        });
+
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+
+    [Fact(DisplayName = "Create lab fails when request is invalid")]
+    public async Task CreateLab_ShouldFail_WhenRequestIsInvalid()
+    {
+        SetCurrentUser(TestData.AdminUser.Id);
+
+        var response = await PostAsync("api/laboratories", new CreateLaboratoryRequest
+        {
+            LaboratoryName = "no" // must be at least 3 characters
+        });
+
+        Assert.Equal(HttpStatusCode.UnprocessableEntity, response.StatusCode);
+    }
+}
---
+++ tests/Beacon.API.IntegrationTests/Endpoints/Laboratories/GetCurrentLabTests.cs
@@ -0,0 +1,26 @@
+﻿using Beacon.Common.Models;
+using System.Net;
+
+namespace Beacon.API.IntegrationTests.Endpoints.Laboratories;
+
+public sealed class GetCurrentLabTests : TestBase
+{
+    public GetCurrentLabTests(TestFixture fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task GetCurrentLab_ReturnsExpectedResult()
+    {
+        SetCurrentUser(TestData.MemberUser.Id);
+
+        var response = await GetAsync("api/laboratories/current");
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var lab = await DeserializeAsync<LaboratoryDto>(response);
+
+        Assert.NotNull(lab);
+        Assert.Equal(TestData.Lab.Id, lab.Id);
+        Assert.Equal(TestData.Lab.Name, lab.Name);
+    }
+}
