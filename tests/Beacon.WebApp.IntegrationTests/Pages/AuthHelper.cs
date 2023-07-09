db2bfb852286db70af8a7b2023b3eb233d7aae6a
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-09 17:31:34 -0400

    wip

--- tests/Beacon.WebApp.IntegrationTests/Pages/AuthHelper.cs
+++ tests/Beacon.WebApp.IntegrationTests/Pages/AuthHelper.cs
@@ -7,6 +7,7 @@ public static class AuthHelper
     public static CurrentUserDto DefaultSession => new
     (
         Id: new Guid("aeaea2c0-ade9-4af9-a0c1-7f49aff0dc54"),
-        DisplayName: "test"
+        DisplayName: "test",
+        MembershipType: LaboratoryMembershipType.Admin
     );
 }
