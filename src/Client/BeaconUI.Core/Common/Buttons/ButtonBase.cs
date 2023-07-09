10dd1aa133dac26c9632939808f53979cfad3b7b
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-09 16:30:20 -0400

    wip

---
+++ src/Client/BeaconUI.Core/Common/Buttons/ButtonBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BeaconUI.Core.Common.Buttons;
+
+public abstract class ButtonBase : ComponentBase
+{
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object?>? AdditionalAttributes { get; set; }
+
+    protected virtual bool IsDisabled { get; }
+
+    protected abstract Task Click();
+}
