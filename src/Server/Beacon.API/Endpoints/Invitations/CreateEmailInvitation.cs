0db73de2b025239f7f3f9292e0db4e425f6e70d0
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-10 16:11:44 -0400

    cleanup

--- src/Server/Beacon.API/Endpoints/Invitations/CreateEmailInvitation.cs
+++ src/Server/Beacon.API/Endpoints/Invitations/CreateEmailInvitation.cs
@@ -5,7 +5,6 @@ using Beacon.App.Services;
 using Beacon.App.Settings;
 using Beacon.Common.Requests.Invitations;
 using Beacon.Common.Services;
-using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -19,30 +18,6 @@ public sealed class CreateEmailInvitation : IBeaconEndpoint
     public static void Map(IEndpointRouteBuilder app)
     {
         app.MapPost<CreateEmailInvitationRequest>("invitations").WithTags(EndpointTags.Invitations);
-    }
-
-    public sealed class Validator : AbstractValidator<CreateEmailInvitationRequest>
-    {
-        private readonly BeaconDbContext _dbContext;
-        private readonly ILabContext _labContext;
-
-        public Validator(BeaconDbContext dbContext, ILabContext labContext)
-        {
-            _dbContext = dbContext;
-            _labContext = labContext;
-
-            RuleFor(r => r.NewMemberEmailAddress)
-                .MustAsync(NotBeAnExistingMember)
-                .WithMessage("User is already a member of the specified laboratory.");
-        }
-
-        private async Task<bool> NotBeAnExistingMember(CreateEmailInvitationRequest request, string email, CancellationToken ct)
-        {
-            var isAMember = await _dbContext.Memberships
-                .AnyAsync(m => m.LaboratoryId == _labContext.CurrentLab.Id && m.Member.EmailAddress == email, ct);
-
-            return !isAMember;
-        }
     }
 
     internal sealed class Handler : IRequestHandler<CreateEmailInvitationRequest>
@@ -60,16 +35,17 @@ public sealed class CreateEmailInvitation : IBeaconEndpoint
 
         public async Task Handle(CreateEmailInvitationRequest request, CancellationToken ct)
         {
-            if (_context.CurrentLab?.MembershipType is not { } type || type < request.MembershipType)
+            if (_context.CurrentLab.MembershipType < request.MembershipType)
                 throw new UserNotAllowedException();
 
-            var user = await _dbContext.Users.SingleAsync(x => x.Id == _context.CurrentUser.Id, ct);
-            var invitation = await CreateInvitation(request, user, ct);
+            await EnsureUserIsNotAnExistingMember(request.NewMemberEmailAddress, ct);
+
+            var invitation = await CreateInvitation(request, ct);
 
             await _publisher.Publish(new EmailInvitationCreated(invitation.Id), ct);
         }
 
-        private async Task<InvitationEmail> CreateInvitation(CreateEmailInvitationRequest request, User currentUser, CancellationToken ct)
+        private async Task<InvitationEmail> CreateInvitation(CreateEmailInvitationRequest request, CancellationToken ct)
         {
             var now = DateTimeOffset.UtcNow;
 
@@ -80,10 +56,8 @@ public sealed class CreateEmailInvitation : IBeaconEndpoint
                 ExpireAfterDays = 10, // TODO: make this configurable
                 NewMemberEmailAddress = request.NewMemberEmailAddress,
                 MembershipType = request.MembershipType,
-                Laboratory = await _dbContext.Laboratories.SingleAsync(x => x.Id == _context.CurrentLab.Id, ct),
                 LaboratoryId = _context.CurrentLab.Id,
-                CreatedById = currentUser.Id,
-                CreatedBy = currentUser
+                CreatedById = _context.CurrentLab.Id
             };
 
             var emailInvitation = invitation.AddEmailInvitation(now);
@@ -93,19 +67,25 @@ public sealed class CreateEmailInvitation : IBeaconEndpoint
 
             return emailInvitation;
         }
+
+        private async Task EnsureUserIsNotAnExistingMember(string newMemberEmail, CancellationToken ct)
+        {
+            if (await _dbContext.Memberships.AnyAsync(m => m.LaboratoryId == _context.CurrentLab.Id && m.Member.EmailAddress == newMemberEmail, ct))
+                throw new BeaconValidationException(nameof(CreateEmailInvitationRequest.NewMemberEmailAddress), "User is already a member of the specified laboratory.");
+        }
     }
 
     public record EmailInvitationCreated(Guid EmailInvitationId) : INotification;
 
     internal sealed class NotificationHandler : INotificationHandler<EmailInvitationCreated>
     {
-        private readonly ApplicationSettings _appSettings;
+        private readonly string _baseUrl;
         private readonly BeaconDbContext _dbContext;
         private readonly IEmailService _emailService;
 
         public NotificationHandler(IOptions<ApplicationSettings> appSettings, BeaconDbContext dbContext, IEmailService emailService)
         {
-            _appSettings = appSettings.Value;
+            _baseUrl = appSettings.Value.BaseUrl;
             _dbContext = dbContext;
             _emailService = emailService;
         }
@@ -127,31 +107,19 @@ public sealed class CreateEmailInvitation : IBeaconEndpoint
         private async Task<IEmailSendOperation> SendAsync(InvitationEmail emailInvitation)
         {
             return await _emailService.SendAsync(
-                GetSubject(emailInvitation.LaboratoryInvitation),
-                GetBody(_appSettings.BaseUrl, emailInvitation),
+                $"{emailInvitation.LaboratoryInvitation.CreatedBy.DisplayName} invites you to join a lab!",
+                GetBody($"{_baseUrl}/invitations/{emailInvitation.Id}/accept", emailInvitation.LaboratoryInvitation),
                 emailInvitation.LaboratoryInvitation.NewMemberEmailAddress)
                 ?? throw new Exception("There was an error sending the invitation email."); // TODO: throw better exception
         }
 
-        private static string GetSubject(Invitation invitation)
+        private static string GetBody(string acceptUrl, Invitation invitation)
         {
-            return $"{invitation.CreatedBy.DisplayName} invites you to join a lab!";
-        }
-
-        private static string GetAcceptUrl(string baseUrl, InvitationEmail invitation)
-        {
-            return $"{baseUrl}/invitations/{invitation.Id}/accept";
-        }
-
-        private static string GetBody(string baseUrl, InvitationEmail emailInvitation)
-        {
-            var invitation = emailInvitation.LaboratoryInvitation;
-
             return $"""
             <p>
                 <h3>You're invited to join {invitation.Laboratory.Name}!</h3>
                 <p>
-                    <a href="{GetAcceptUrl(baseUrl, emailInvitation)}">Click here to accept.</a>
+                    <a href="{acceptUrl}">Click here to accept.</a>
                     <br />
                     <small>This invitation will expire in {invitation.ExpireAfterDays} days.</small>
                 </p>
