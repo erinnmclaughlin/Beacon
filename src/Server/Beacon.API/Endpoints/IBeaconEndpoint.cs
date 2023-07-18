32ad3284dddb5bbe61612cbd32f4fd461e772a15
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-18 07:36:53 -0400

    wip

--- src/Server/Beacon.API/Endpoints/IBeaconEndpoint.cs
+++ src/Server/Beacon.API/Endpoints/IBeaconEndpoint.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Routing;
+﻿using Beacon.Common.Requests.Auth;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using System.Reflection;
 
 namespace Beacon.API.Endpoints;
@@ -12,13 +16,40 @@ internal static class EndpointMapper
 {
     public static void MapBeaconEndpoints(this IEndpointRouteBuilder app)
     {
-        var endpoints = Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => t.GetInterfaces().Any(i => i == typeof(IBeaconEndpoint)));
+        var requestTypes = typeof(LoginRequest).Assembly.GetTypes()
+            .Where(t => t.GetInterfaces().Any(i => i.IsAssignableTo(typeof(IBaseRequest))));
 
-        foreach (var endpoint in endpoints)
+        foreach (var requestType in requestTypes)
         {
-            var mapMethod = endpoint.GetMethod(nameof(IBeaconEndpoint.Map), types: new[] { typeof(IEndpointRouteBuilder) });
-            mapMethod!.Invoke(null, new object[] { app });
+
         }
+
+        //var endpoints = Assembly.GetExecutingAssembly().GetTypes()
+        //    .Where(t => t.GetInterfaces().Any(i => i == typeof(IBeaconEndpoint)));
+
+        //foreach (var endpoint in endpoints)
+        //{
+        //    var mapMethod = endpoint.GetMethod(nameof(IBeaconEndpoint.Map), types: new[] { typeof(IEndpointRouteBuilder) });
+        //    mapMethod!.Invoke(null, new object[] { app });
+        //}
+    
+    }
+
+    public static void Map<TRequest>(IEndpointRouteBuilder app) where TRequest : IRequest
+    {
+        app.MapPost($"api/{typeof(TRequest).Name}", async (TRequest request, IMediator m, CancellationToken ct) =>
+        {
+            await m.Send(request, ct);
+            return Results.NoContent();
+        });
+    }
+
+    public static void Map<TRequest, TResponse>(IEndpointRouteBuilder app) where TRequest : IRequest<TResponse>
+    {
+        app.MapPost($"api/{typeof(TRequest).Name}", async (TRequest request, IMediator m, CancellationToken ct) =>
+        {
+            var response = await m.Send(request, ct);
+            return Results.Ok(response);
+        });
     }
 }
