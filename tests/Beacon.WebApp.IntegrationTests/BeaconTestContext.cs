f51bb45289cd58307eba8430891dd3387c4acb1c
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-22 16:58:23 -0400

    remove unused thing

--- tests/Beacon.WebApp.IntegrationTests/BeaconTestContext.cs
+++ tests/Beacon.WebApp.IntegrationTests/BeaconTestContext.cs
@@ -27,7 +27,6 @@ public abstract class BeaconTestContext : TestContext
         JSInterop.SetupModule("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
     }
 
-    protected BeaconAuthStateProvider AuthStateProvider => Services.GetRequiredService<BeaconAuthStateProvider>();
     protected Mock<IApiClient> MockApi => Services.GetRequiredService<Mock<IApiClient>>();
     protected FakeNavigationManager NavigationManager => Services.GetRequiredService<FakeNavigationManager>();
 
