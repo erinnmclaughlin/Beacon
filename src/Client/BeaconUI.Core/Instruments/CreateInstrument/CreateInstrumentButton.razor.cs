ad69f80958a817964601578951feb05495eb3278
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-23 19:57:45 -0400

    ui stuff

---
+++ src/Client/BeaconUI.Core/Instruments/CreateInstrument/CreateInstrumentButton.razor.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BeaconUI.Core.Instruments.CreateInstrument;
+
+public partial class CreateInstrumentButton
+{
+    [CascadingParameter]
+    public required IModalService ModalService { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object?>? AdditionalAttributes { get; set; }
+
+    [Parameter]
+    public EventCallback OnInstrumentCreated { get; set; }
+
+    private async Task ShowCreateInstrumentModal()
+    {
+        var result = await ModalService.Show<CreateInstrumentModal>("Create Instrument").Result;
+
+        if (result.Confirmed)
+            await OnInstrumentCreated.InvokeAsync();
+    }
+}
---
+++ src/Client/BeaconUI.Core/Instruments/CreateInstrument/CreateInstrumentModal.razor.cs
@@ -0,0 +1,25 @@
+﻿using Beacon.Common.Requests.Instruments;
+using BeaconUI.Core.Common.Forms;
+using BeaconUI.Core.Common.Http;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BeaconUI.Core.Instruments.CreateInstrument;
+
+public partial class CreateInstrumentModal
+{
+    [Inject]
+    private IApiClient ApiClient { get; set; } = default!;
+
+    [CascadingParameter]
+    private BlazoredModalInstance Modal { get; set; } = default!;
+
+    private CreateLaboratoryInstrumentRequest Request { get; } = new();
+
+    private async Task Submit(BeaconForm formContext)
+    {
+        await ApiClient.SendAsync(Request);
+        await Modal.CloseAsync(ModalResult.Ok());
+    }
+}
