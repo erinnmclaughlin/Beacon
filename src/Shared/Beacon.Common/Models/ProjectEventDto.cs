f56d3b1592ba9a9cf7e08aba32cbafa0b8f7c93a
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-07-30 11:32:27 -0400

    display total duration

--- src/Shared/Beacon.Common/Models/ProjectEventDto.cs
+++ src/Shared/Beacon.Common/Models/ProjectEventDto.cs
@@ -9,6 +9,8 @@ public sealed record ProjectEventDto
     public required DateTimeOffset ScheduledEnd { get; init; }
     public required LaboratoryInstrumentDto[] AssociatedInstruments { get; init; }
 
+    public TimeSpan GetDuration() => ScheduledEnd - ScheduledStart;
+
     public bool IsOngoingOn(DateTime timestamp)
     { 
         return ScheduledStart.DateTime <= timestamp && ScheduledEnd.DateTime > timestamp;
