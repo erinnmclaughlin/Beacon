ab149cca59064ae1a8ad3f43cb60468c5ba34dad
Erin McLaughlin <erinmclaughlin219@gmail.com>
2023-08-13 18:29:41 -0400

    merge project overview & project schedule tabs

--- src/Client/BeaconUI.Core/Projects/ProjectOverview.razor.cs
+++ src/Client/BeaconUI.Core/Projects/ProjectOverview.razor.cs
@@ -1,37 +1,13 @@
-using Beacon.Common;
 using Beacon.Common.Models;
-using Beacon.Common.Requests.Laboratories;
-using BeaconUI.Core.Common.Http;
-using ErrorOr;
 using Microsoft.AspNetCore.Components;
 
 namespace BeaconUI.Core.Projects;
 
 public partial class ProjectOverview
 {
-    [Inject]
-    private IApiClient ApiClient { get; set; } = default!;
-
     [Parameter]
     public required ProjectDto Project { get; set; }
 
     [Parameter]
     public required EventCallback<ProjectDto> ProjectChanged { get; set; }
-
-    private ErrorOr<PagedList<LaboratoryEventDto>>? Events { get; set; }
-
-    protected override async Task OnParametersSetAsync()
-    {
-        await LoadEvents();
-    }
-
-    private async Task LoadEvents()
-    {
-        Events = await ApiClient.SendAsync(new GetLaboratoryEventsRequest
-        {
-            MinEnd = DateTime.UtcNow,
-            MaxStart = DateTime.UtcNow.AddDays(7),
-            ProjectIds = new() { Project.Id }
-        });
-    }
 }
